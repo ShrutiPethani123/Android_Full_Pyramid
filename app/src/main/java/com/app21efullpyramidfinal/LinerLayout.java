3f20ac7b7149eb7734591b617dc175e05df7d663
ShrutiPethani123 <shrutipethani.royal@gmail.com>
2023-02-20 11:54:57 +0530

    login Activity

---
+++ app/src/main/java/com/app21efullpyramidfinal/LinerLayout.java
@@ -0,0 +1,14 @@
+package com.app21efullpyramidfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LinerLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_liner_layout);
+    }
+}
