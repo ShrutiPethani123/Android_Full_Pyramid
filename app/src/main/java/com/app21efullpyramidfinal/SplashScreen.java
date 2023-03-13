b561d37badfcaa64664d0dee2a99a3e01e750c26
ShrutiPethani123 <shrutipethani.royal@gmail.com>
2023-03-13 19:40:48 +0530

    splash Activity

---
+++ app/src/main/java/com/app21efullpyramidfinal/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.app21efullpyramidfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        int time=3000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this,LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },time);
+
+    }
+}
